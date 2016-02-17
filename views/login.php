1196211cd3ac13c60a9a71ff099e24e4bddb4b2a
Dheeraj <dheeraj.jha@winculum.com>
2016-02-17 13:08:16 +0530

    Codeigniter 3.0 Linkedin Login

---
+++ views/login.php
@@ -0,0 +1,3 @@
+<form id="linkedin_connect_form" action="<?php echo base_url('linkedin_signup/initiate')?>" method="post">
+		<button class="btn btn-primary btn-block"><i class="fa fa-linkedin-square fa-lg"></i> Login with Linkedin</button>
+</form>
